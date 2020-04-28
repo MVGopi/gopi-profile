4a3cee4878413b8867b1f0b579cd80b760267a55
saisaira <saitarakdevarapalli@gmail.com>
2020-04-29 01:42:45 +0530

    Updates

---
+++ precache-manifest.96390620dca4b85b802844bbd47bcfc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44448a854a053c563c684c17d555564",
+    "url": "/gopi-profile/index.html"
+  },
+  {
+    "revision": "4a81c2955fad051aab61",
+    "url": "/gopi-profile/static/css/main.575f0ec9.chunk.css"
+  },
+  {
+    "revision": "bc2810a2d40a0d0fc1aa",
+    "url": "/gopi-profile/static/js/2.539345e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gopi-profile/static/js/2.539345e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a81c2955fad051aab61",
+    "url": "/gopi-profile/static/js/main.ba87edc9.chunk.js"
+  },
+  {
+    "revision": "b34cb829b0bc473b66f6",
+    "url": "/gopi-profile/static/js/runtime-main.1bb94307.js"
+  },
+  {
+    "revision": "408505dcd076f385077cde8d2a13813d",
+    "url": "/gopi-profile/static/media/mypic.408505dc.jpg"
+  }
+]);
